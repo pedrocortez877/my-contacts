f7c067bcf7e14aba5b07f19860ffb7df7ccf6eeb
Pedro Cortez <pedrocortez877@gmail.com>
2023-04-18 22:50:18 -0300

    feat: input and select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/input';
+import Select from '../../components/select';
 
 export default function Home() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
