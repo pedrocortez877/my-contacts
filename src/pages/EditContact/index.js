1ccaf5d8b50bcb90c7089a76c2169baeb8e4aa47
Pedro Cortez <pedrocortez877@gmail.com>
2023-05-03 20:04:02 -0300

    feat: modal

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,5 +1,11 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function Home() {
-  return <PageHeader title="Editar Mateus Silva" />;
+  return (
+    <>
+      <PageHeader title="Editar Mateus Silva" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
+  );
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,20 +1,12 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
-import Button from '../../components/button';
-import Input from '../../components/input';
-import Select from '../../components/select';
 
 export default function Home() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <Input placeholder="Nome" />
-      <Select>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-        <option value="123">Instagram</option>
-      </Select>
-      <Button type="button">Salvar alterações</Button>
-      <Button type="button" disabled>Salvar alterações</Button>
+
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
